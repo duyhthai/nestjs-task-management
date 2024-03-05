21f9ffdf387f142c114da5f9724cf802ca17e818
Duy Thai <Duy.Thai@altsourcesoftware.com>
2024-03-05 23:19:05 +0700

    error handling: duplicate username

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -13,6 +17,15 @@ export class UsersRepository extends Repository<User> {
     const { username, password } = authCredentialsDto;
 
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      }
+
+      throw new InternalServerErrorException();
+    }
   }
 }
